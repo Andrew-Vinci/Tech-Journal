49c0105fe3b340f13d326d9a393df6ef6700d42b
Andrew-Vinci <78275653+Andrew-Vinci@users.noreply.github.com>
2021-05-02 20:48:38 -0400

    Add files via upload

--- VideoGame_SoftwareJava.js
+++ VideoGame_SoftwareJava.js
@@ -1,4 +1,6 @@
 var api_start = "https://api.cryptonator.com/api/full/";
+var api_start_new  = "https://api.coinlore.net/api/ticker/?id=90";
+var api_start_new2  = "https://api.coinlore.net/api/ticker/?id=80";
 
     function refreshPage(){
         window.location.reload();
@@ -68,6 +70,46 @@ var api_start = "https://api.cryptonator.com/api/full/";
             document.getElementById("volume3").innerHTML = (JSON.stringify(response.ticker.volume));
         });
         }
+
+        function coinDisplayNew(){
+            fetch(api_start_new)
+            .then(data => {
+                return data.json();
+            })
+            .then(response => {
+                console.log(response);
+                console.log(JSON.stringify(response[0].name));
+                document.getElementById("name1").innerHTML = (JSON.stringify(response[0].name));
+                console.log(JSON.stringify(response[0].price_usd));
+                document.getElementById("price1").innerHTML = (JSON.stringify(response[0].price_usd));
+                console.log(JSON.stringify(response[0].market_cap_usd));
+                document.getElementById("marketCap").innerHTML = (JSON.stringify(response[0].market_cap_usd));
+                console.log(JSON.stringify(response[0].percent_change_1h));
+                document.getElementById("hourChange").innerHTML = (JSON.stringify(response[0].percent_change_1h));
+                console.log(JSON.stringify(response[0].percent_change_7d));
+                document.getElementById("weekChange").innerHTML = (JSON.stringify(response[0].percent_change_7d));
+            });
+            };
+
+            function coinDisplayNew2(){
+                fetch(api_start_new2)
+                .then(data => {
+                    return data.json();
+                })
+                .then(response => {
+                    console.log(response);
+                    console.log(JSON.stringify(response[0].name));
+                    document.getElementById("name21").innerHTML = (JSON.stringify(response[0].name));
+                    console.log(JSON.stringify(response[0].price_usd));
+                    document.getElementById("price21").innerHTML = (JSON.stringify(response[0].price_usd));
+                    console.log(JSON.stringify(response[0].market_cap_usd));
+                    document.getElementById("marketCap2").innerHTML = (JSON.stringify(response[0].market_cap_usd));
+                    console.log(JSON.stringify(response[0].percent_change_1h));
+                    document.getElementById("hourChange2").innerHTML = (JSON.stringify(response[0].percent_change_1h));
+                    console.log(JSON.stringify(response[0].percent_change_7d));
+                    document.getElementById("weekChange2").innerHTML = (JSON.stringify(response[0].percent_change_7d));
+                });
+                };
 
 
             
